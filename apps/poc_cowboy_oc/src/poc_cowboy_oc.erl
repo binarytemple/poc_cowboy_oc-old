ce036ba0d00169281fa3f5bb0772829f5251d1ef
bryan <bryan.hunt@erlang-solutions.com>
2019-07-16 20:07:54 +0100

    kill spawned functions after 60 seconds

--- apps/poc_cowboy_oc/src/poc_cowboy_oc.erl
+++ apps/poc_cowboy_oc/src/poc_cowboy_oc.erl
@@ -9,18 +9,28 @@
 
 -define(TRACE_OUTFILE, (?OUTDIR) ++ "out.trace").
 
+-define('1_MINUTE',60 * 1000).
+-define(SPAWN_COUNT, 1000).
+
 spawn_lots() ->
-    Pids = lists:map(
-      fun (_) ->
-			  erlang:spawn(poc_cowboy_oc_gen_server, start, [])
-		  end, 
-    lists:seq(1, 10000)),
-    lists:foreach(fun(P) -> P ! gen_prime end , Pids   )
-		 .
+    Pids = [spawn_lots_1(V1) || V1 <- lists:seq(1, ?SPAWN_COUNT)],
+    lists:foreach(fun (P) -> P ! gen_prime end, Pids),
+    erlang:spawn(fun () ->
+			 receive
+			   _ -> ok
+			   after ?'1_MINUTE' ->
+				     lists:foreach(fun (P) ->
+							   erlang:exit(P, kill)
+						   end,Pids)
+			 end
+		 end).
+
+spawn_lots_1(_) ->
+    erlang:spawn(poc_cowboy_oc_gen_server, start, []).
 
 export_trace() ->
     eflame2:format_trace(?TRACE_OUTFILE,
-			 (?OUTDIR) ++ file_timestamp() ++ ".flame") .
+			 (?OUTDIR) ++ file_timestamp() ++ ".flame").
 
 file_timestamp() ->
     {{Y, Mo, D}, {H, Mi, S}} =
@@ -40,5 +50,3 @@ run_eflame_global_calls_plus_new_procs(Milliseconds) ->
 					  ?TRACE_OUTFILE, all, Milliseconds),
 		  io:format("Tracing finished!\n")
 	  end).
-
-
--- apps/poc_cowboy_oc/src/poc_cowboy_oc_gen_server.erl
+++ apps/poc_cowboy_oc/src/poc_cowboy_oc_gen_server.erl
@@ -5,11 +5,15 @@
 -export([code_change/3, handle_call/3, handle_cast/2,
 	 handle_info/2, init/1, terminate/2]).
 
--export([sleep/1, gen_primes/1, start/0]).
+-export([gen_primes/1, sleep/1, start/0]).
+
+%% arbitrarily test the run function
+-export([run/1]).
 
 start() -> gen_server:start(?MODULE, [], []).
 
 sleep(pid) -> gen_server:call(pid, sleep).
+
 gen_primes(pid) -> gen_server:call(pid, gen_primes).
 
 init(_Args) -> {ok, []}.
@@ -18,10 +22,9 @@ handle_call(sleep, _From, State) ->
     {reply, State, State}.
 
 handle_cast(gen_primes, State) ->
-  erlang:display("gen_primes"),
-    run(1000000000000000000000000000000),
+    erlang:display("gen_primes"),
+    run(10000000000000000000000000000),
     {reply, State, State};
-
 handle_cast(_Request, State) -> {noreply, State}.
 
 handle_info(_Info, State) -> {noreply, State}.
@@ -30,17 +33,19 @@ terminate(_Reason, _State) -> ok.
 
 code_change(_OldVsn, State, _Extra) -> {ok, State}.
 
-
 % generate primes - lets burn some cpu
 
-sieve1([H|T], S) when H =< S -> [H|sieve1([X||X<-T, X rem H /= 0], S)];
+sieve1([H | T], S) when H =< S ->
+    [H | sieve1([X || X <- T, X rem H /= 0], S)];
 sieve1(L, _) -> L.
 
 filter(_, [], A) -> lists:reverse(A);
-filter(P, [H|T], A) when H rem P == 0 -> filter(P, T, A);
-filter(P, [H|T], A) -> filter(P, T, [H|A]).
+filter(P, [H | T], A) when H rem P == 0 ->
+    filter(P, T, A);
+filter(P, [H | T], A) -> filter(P, T, [H | A]).
 
-sieve2([H|T], S) when H =< S -> [H|sieve2(filter(H, T, []), S)];
+sieve2([H | T], S) when H =< S ->
+    [H | sieve2(filter(H, T, []), S)];
 sieve2(L, _) -> L.
 
 run(N) ->
@@ -49,6 +54,4 @@ run(N) ->
     % erlang:display({p1, now()}),
     _P2 = sieve2(lists:seq(3, N), math:sqrt(N)),
     % erlang:display({p2, now()})
-true  
-.
-
+    true.
